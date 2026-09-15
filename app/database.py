9982d03095b7a0df7dbdec07247bdf8cdbf16ffc
Junaid <06Junaidsheikh>
2026-09-15 18:52:56 +0500

    Update .gitignore to exclude environment files and remove unnecessary entries; refactor database connection to load DATABASE_URL from .env

--- app/database.py
+++ app/database.py
@@ -1,7 +1,13 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg://postgres:Postgres123@localhost/taskflow"
+
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
